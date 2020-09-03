1eb07ad784e033941e959afcca2adb4f0d60bab0
angelina-tsuboi <angelina.t1832@gmail.com>
2020-09-02 18:19:29 -0700

    set up defaults

---
+++ src/main/java/com/udacity/jwdnd/todo/Counter/ResetForm.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.todo.Counter;
+
+public class ResetForm {
+    private Integer value;
+
+    public Integer getValue(){
+        return value;
+    }
+
+    public void setValue(Integer value){
+        this.value = value;
+    }
+}
